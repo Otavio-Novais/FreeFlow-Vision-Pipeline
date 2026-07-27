8ed6f10dcec2fc38c1af42e236716b3878f55fa5
Otavio Novais <otavioaugustonv@gmail.com>
2026-07-27 11:27:14 -0300

    Refactor requirements.txt to streamline dependencies and update inference.py with enhanced comments for clarity in vehicle detection logic.

--- src/inference.py
+++ src/inference.py
@@ -32,24 +32,28 @@ class VehicleDetector:
         Returns:
             Lista de dicionários com as declarações
         """
+
+        # Carregamos o modelo e pedimos para ele predizer a imagem passada
         results = self.model.predict(
-            source= image_path,
+            source= image_path, 
             conf=self.conf_threshold,
-            iou = 0.30,
+            iou = 0.30, # Sobreposição mínima para ser considerada um acerto.
             save = save_results,
             project=str(OUTPUTS_DIR),
-            name='predictions',
+            name='predictions', # Pasta que iremos salvar os resultados
             exist_ok=True,
             verbose=False
         )
 
-        detections = []
+        detections = [] 
 
+        # Para cara resultado gerado pelo modelo iremos puxar as informações geradas pelo modelo
+        #Como por exemplo, a classe, confiança no que aponta, localização do box gerado
         for result in results:
             if result.boxes is not None:
                 boxes = result.boxes
-                classes = boxes.cls.cpu().numpy().astype(int)
-                confidences = boxes.conf.cpu().numpy()
+                classes = boxes.cls.cpu().numpy().astype(int) # Utilizamos do .cpu(), caso o processamento rodar em um GPU, só poderemos acessar a informação caso ela estiver na memória da CPU.
+                confidences = boxes.conf.cpu().numpy() # Utilizamos do .numpy() porque mesmo após trazer os dados da GPU para a CPU, esse objeto está no tipo Tensor
                 xyxy = boxes.xyxy.cpu().numpy()
 
                 for i in range(len(boxes)):
@@ -76,17 +80,17 @@ class VehicleDetector:
         x1,y1,x2,y2 = map(int,bbox)
 
         h,w = img.shape[:2]
-        x1 = max(0, min(x1,w))
-        y1 = max(0, min(y1,h))
-        x2 = max(0, min(x2,w))
+        x1 = max(0, min(x1,w)) # Utilizamos do min() para garantir que o modelo não ultrapasse do tamanho real da imagem (modelo diz que x1 está em 1050 mas o tamanho da imagem é no máximo 1000)
+        y1 = max(0, min(y1,h)) # Utilizamos do max() para evitar que o modelo diga que a placa começa em pixel inexistentes (modelo diz que y1 começa no -20)
+        x2 = max(0, min(x2,w)) # Esses casos podem acontecer devido a natureza de regressão matemática aplicada, objetos que estão na borda correm esse risco. 
         y2 = max(0, min(y2,h))
 
-        cropped = img[y1:y2, x1:x2]
+        cropped = img[y1:y2, x1:x2] # Com base no box predito pelo nosso modelo acima, estamos recortando a imagem 
         return cropped
 
 
 
-
+# Script de teste:
 if __name__ == '__main__':
     # 1. Criar detector com threshold mais alto para produção
     detector = VehicleDetector(conf_threshold=0.05)
